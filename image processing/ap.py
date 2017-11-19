8402cf97b90918e80a57c41bd37ff35062eb1211
Mayank_S05 <mayank.singh0597@gmail.com>
2017-11-19 18:27:57 +0530

    Update for A , B, C, D, Del , Space , Nothing Recognition

--- image processing/ap.py
+++ image processing/ap.py
@@ -1,17 +1,34 @@
 from flask import Flask, render_template, Response
 from camera import VideoCamera
+# from flask_socketio import SocketIO, emit
+
 
 app = Flask(__name__)
+var='no'
+string = "q"
+count = 0
+# app.config[ 'SECRET_KEY' ] = 'jsbcfsbfjefebw237u3gdbdc'
+# socketio = SocketIO( app )
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',var=var)
 
 def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+    tf = camera.get_tf()
+    with tf.Session() as sess:
+    	while True:
+        	frame,var = camera.get_frame(sess)
+        	yield (b'--frame\r\n'
+             		b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        	print(var)
+        	# if(var != 'no'):
+        	# 	if(var == 'a' or var == 'b' or var == 'c' or var == 'd'):
+		       #      print(var , end='')
+		       #  elif(var == 'space'):
+		       #      print(' ', end='')
+		       #  else:
+		       #      print('',end='')
 
 @app.route('/video_feed')
 def video_feed():
@@ -19,4 +36,5 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    	app.run(host='127.0.0.1', debug=True, port=5000)
+	# socketio.run( app, debug = True )
+	app.run(host='127.0.0.1', debug=True, port=5000)
--- image processing/camera.py
+++ image processing/camera.py
@@ -1,49 +1,108 @@
 import cv2
 import numpy as np
+import sys
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+import copy
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+
 
 class VideoCamera(object):
     def __init__(self):
         # Using OpenCV to capture from device 0. If you have trouble capturing
         # from a webcam, comment the line below out and use a video file
         # instead.
+
+        self.res = 0.0
+        self.score = ''
+        self.i = 0
+        self.mem = ''
+        self.consecutive = 0
+        self.sequence = ''
+        
         self.video = cv2.VideoCapture(0)
-        # If you decide to use video.mp4, you must have this file in the folder
+        self.label_lines = [line.rstrip() for line in tf.gfile.GFile("logs/trained_labels.txt")]
+        
+        with tf.gfile.FastGFile("logs/trained_graph.pb", 'rb') as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+            _ = tf.import_graph_def(graph_def, name='')
+
+# If you decide to use video.mp4, you must have this file in the folder
         # as the main.py.
         # self.video = cv2.VideoCapture('video.mp4')
-    
+    def get_tf(self):
+        return tf
+
     def __del__(self):
         self.video.release()
     
-    def get_frame(self):
-        fist_cascade = cv2.CascadeClassifier('fist.xml')
-        fgbg = cv2.createBackgroundSubtractorMOG2()
+    def predict(self,image_data,sess,softmax_tensor):
+        predictions = sess.run(softmax_tensor, \
+                 {'DecodeJpeg/contents:0': image_data})
 
-        success, image = self.video.read()
-        fist_roi = image[100:300 , 100:300]
-        hsv = cv2.cvtColor(fist_roi , cv2.COLOR_BGR2HSV)
-        lower_skin = np.array([0,48,80] , dtype="uint8")
-        upper_skin = np.array([20,255,255] , dtype="uint8")
-        mask = cv2.inRange(hsv , lower_skin , upper_skin)
-        res = cv2.bitwise_and(fist_roi , fist_roi , mask=mask)
-        fgmask = fgbg.apply(res)
-        median = cv2.medianBlur(res,15)
+        # Sort to show labels of first prediction in order of confidence
+        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
 
-        fist_gray = cv2.cvtColor(fist_roi , cv2.COLOR_BGR2GRAY)
-         #faces = face_cascade.detectMultiScale(gray)
-        fist = fist_cascade.detectMultiScale(fist_gray)
-        cv2.rectangle(image ,(100,100) , (300,300) , (255,0,0),2)
+        max_score = 0.0
+        res = ''
+        for node_id in top_k:
+            human_string = self.label_lines[node_id]
+            self.score = predictions[0][node_id]
+            if self.score > max_score:
+                max_score = self.score
+                res = human_string
+        return res, max_score    
     
-        for(x,y,w,h) in fist:
-            cv2.rectangle(image , (100+x,100+y), (100+x+w , 100+y+h) , (0,0,255), 2)
-            roi_gray = fist_gray[y:y+h , x:x+w]
-            roi_color = fist_roi[y:y+h , x:x+w]
-            #roi_fist = img[y:y+h+20 , x:x+w+20]
-            #if roi_fist is not None:
-            #   cv2.imshow('roi' , roi_fist)
+    def get_frame(self,sess):
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+        ret, img = self.video.read()
+        if ret:
+            x1, y1, x2, y2 = 100, 100, 300, 300
+            img_cropped = img[y1:y2, x1:x2]
+
+            # c += 1
+            image_data = cv2.imencode('.jpg', img_cropped)[1].tostring()
+            a = cv2.waitKey(33)
+            if a == 27:
+                return
+            if self.i == 4:
+                res_tmp, self.score = self.predict(image_data,sess,softmax_tensor)
+                self.res = res_tmp
+                self.i = 0
+                if self.mem == self.res:
+                    self.consecutive += 1
+                else:
+                    self.consecutive = 0
+                if self.consecutive == 2 and self.res not in ['nothing']:
+                    if self.res == 'space':
+                        self.sequence += ' '
+                    elif self.res == 'del':
+                        self.sequence = self.sequence[:-1]
+                    else:
+                        self.sequence += self.res
+                    self.consecutive = 0
+            self.i += 1
+            # cv2.putText(img, '%s' % (res.upper()), (100,400), cv2.FONT_HERSHEY_SIMPLEX, 4, (255,255,255), 4)
+            # cv2.putText(img, '(score = %.5f)' % (float(score)), (100,450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))
+            # mem = res
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255,0,0), 2)
+            # cv2.imshow("img", img)
+            # img_sequence = np.zeros((200,1200,3), np.uint8)
+            # cv2.putText(img_sequence, '%s' % (sequence.upper()), (30,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+            # cv2.imshow('sequence', img_sequence)
+        else:
+            return       
+        # return "yes"
+        # print(self.score)
         
-        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
-        # video stream.
-        
-        ret, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
+        ret, jpeg = cv2.imencode('.jpg', img)
+        if self.score != '':
+            if float(self.score) > 0.70 :
+                return jpeg.tobytes() , self.res
+            else:
+                return jpeg.tobytes() , 'no'
+        else:
+            return jpeg.tobytes() , 'no'
